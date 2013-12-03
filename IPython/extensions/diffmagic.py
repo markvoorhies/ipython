216ea233413a20061e6c77863798d449be6200c9
Mark Voorhies <mark.voorhies@ucsf.edu>
2013-12-02 19:53:54 -0800

    Add support for cell labels (as ##label:MyLabel) and default to ultimate and penultimate cells

--- IPython/extensions/diffmagic.py
+++ IPython/extensions/diffmagic.py
@@ -11,8 +11,7 @@ Usage
 
 ``%diff cell1 cell2``
 
-Render line-based diff of cell1 and cell2.
-
+{DIFF_DOC}
 """
 
 #-----------------------------------------------------------------------------
@@ -28,6 +27,7 @@ from IPython.core.magic import Magics, magics_class, line_magic
 from IPython.core.displaypub import publish_display_data
 
 from difflib import Differ
+import re
 
 @magics_class
 class DiffMagics(Magics):
@@ -45,18 +45,54 @@ class DiffMagics(Magics):
 
         self.differ = Differ()
 
+    def _get_content(self, key):
+        try:
+            key = int(key)
+            return self.shell.history_manager.get_range(0,key,key+1).next()[2]
+        except ValueError:
+            key_re = re.compile("^[\s]*##label:%s([\s]+.*)?$" % key,re.M)
+            for i in reversed(list(self.shell.history_manager.get_range(0))):
+                if(key_re.search(i[2]) is not None):
+                    return i[2]
+            raise KeyError, "Invalid cell label %s" % key
+
     @line_magic
     def diff(self, line):
-        """Testing
+        """Render line-based diff of cell1 and cell2, where cell1 and cell2
+        are the first two whitespace delimited words in line.
+        
+        If cell2 is not given, it is the ultimate cell.
+
+        If cell1 is not given, it is the penultimate cell.
+
+        Cell names may be integers, in which case they correspond to
+        cell numbers in the kernel's history.
+
+        Otherwise, cell name "x" references the highest numbered cell
+        with first line ##label:x (N.B.: this is a place-holder, pending
+        a formal system for tagging cells).
         """
 
-        vals = map(int, line.split())[:2]
+        keys = line.split()[:2]
 
-        content = [self.shell.history_manager.get_range(0,i,i+1).next()[2]
-                   for i in vals]
+        if(len(keys) > 0):
+            cell1 = self._get_content(keys[0])
+            if(len(keys) > 1):
+                cell2 = self._get_content(keys[1])
+            else:
+                h = reversed(list(self.shell.history_manager.get_range(0)))
+                # burn current cell
+                dummy = h.next()
+                cell2 = h.next()[2]
+        else:
+            assert(self.shell.history_length > 1)
+            h = reversed(list(self.shell.history_manager.get_range(0)))
+            # burn current cell
+            dummy = h.next()
+            cell2 = h.next()[2]
+            cell1 = h.next()[2]
 
-        diff = self.differ.compare(content[0].splitlines(),
-                                   content[1].splitlines())
+        diff = self.differ.compare(cell1.splitlines(),cell2.splitlines())
 
         publish_display_data('DiffMagic.diff',{'text/plain':"\n".join(diff)})
 
